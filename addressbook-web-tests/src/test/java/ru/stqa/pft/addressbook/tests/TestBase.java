d888726c1fd5c8ae50aeec4deb3d2638b120f5fa
Antonius1 <antoniustester@gmail.com>
2018-02-25 21:33:45 +0200

    замена наследования на делегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicatoinManager;
+
+public class TestBase {
+
+  protected final ApplicatoinManager app = new ApplicatoinManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
+
+}
