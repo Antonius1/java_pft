d50e1151539b74cd90fe0ee11fabcf3ee236b74f
Antonius1 <antoniustester@gmail.com>
2018-06-04 00:33:32 +0300

    Не отключается каптча

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -13,7 +13,7 @@ public class RegistrationHelper extends HelperBase {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    click(By.cssSelector("input[value='Sinup']"));
+    click(By.cssSelector("input[value='Signup']"));
 
   }
 
